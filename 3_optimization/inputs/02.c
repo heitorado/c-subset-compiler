36e9567392accb964344cc5e46f9672f3b81f3ce
Heitor Carvalho <hscs@cin.ufpe.br>
2020-11-07 22:13:49 -0300

    Add initial files for project part 3

---
+++ 3_optimization/inputs/02.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main () {
+	int a = 3 * 5 - 2 + 1;
+
+	int b = 4 + a * 3;
+
+	int c = a * b * - 1;
+
+	int d = c * a + b * c - a;
+
+	int e = a - b + c * -d;
+	return 0;
+}
+
+
